3ab7e6ae8a064db01392c524a1d3ab01f7948ca0
Alouiscious Media <info@reneesoul.com>
2020-03-26 04:59:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+
+
+class Polygon {
+  constructor(arr) {
+    this.arr = arr
+    this.count = this.arr.length
+  }
+  get countSides() {
+    return this.arr.length 
+    
+  }
+
+  get perimeter(){
+    if (!Array.isArray(this.arr)) return;
+    let perim = 0
+    this.arr.map((side) => {
+      perim = perim + side
+    })
+    return perim
+  }
+}
+
+class Triangle extends Polygon {
+
+  get isValid() {
+    if (!Array.isArray(this.arr) || this.countSides !== 3) return;
+    let sideA = this.arr[0]
+    let sideB = this.arr[1]
+    let sideC = this.arr[2]
+    return ((sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA))
+     
+  }
+}
+
+class Square extends Polygon {
+  
+  get isValid() {
+    if (!Array.isArray(this.arr) || this.countSides !== 4) return;
+    let sideA = this.arr[0]
+    let sideB = this.arr[1]
+    let sideC = this.arr[2]
+    let sideD = this.arr[3]
+    
+    return (sideA == sideB && sideB == sideC && sideC == sideD);
+  }
+  
+  get area() {
+    if (!Array.isArray(this.arr) || this.countSides !== 4) return;
+    let sideA = this.arr[0]
+    let sideB = this.arr[1]
+
+    return sideA * sideB
+  }
+}
+
+
+
